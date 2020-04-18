c165200731f9971e2a7aa8aa5cbc882ac5e4dac2
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-18 15:47:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,6 +16,6 @@ def age_appropriate_birthday(birthday_kids)
     if kids_name == :Amanda
       return "You are too old for this"
     else
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      return "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
 end
