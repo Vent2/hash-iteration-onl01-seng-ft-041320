a5747235901934103ff869405066f6d19c52dd73
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-18 16:03:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.map do |name, age|
-    if name == "Amanda" && age > 9 
+    if name == "Amanda" && age > 9
       return "You are too old for this."
     else
       return "Happy Birthday #{kids_name}! You are now #{age} years old!"
